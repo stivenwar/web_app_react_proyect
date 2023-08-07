0588d13ac1bae5faa406416237733d520fa034a7
esteban <estebanguerra1990@gmail.com>
2023-08-07 18:03:04 +0200

    build app

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
                         <BsFillMoonStarsFill />
                     </li>
                     <li>
+
                         <a className="bg-gradient-to-r bg-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8" href="/#">Resume</a>
                     </li>
                 </ul>
