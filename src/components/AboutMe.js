92347f4f592e36bf8ea984b6ed112cae1b78d82b
esteban <estebanguerra1990@gmail.com>
2023-08-10 05:26:25 +0200

    react app new update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,10 @@
 function AboutMe(){
     return (
         <div className="text-center p-10">
-            <h2 className="text-5xl pb-10  text-blue-500 font-medium md:text-6xl">Esteban guerra</h2>
-            <h3 className="text-2xl py-2 dark:text-white">Desarrollador de aplicaciones y web</h3>
+            <h2 className="text-5xl pb-10  text-blue-500 font-medium md:text-6xl">Esteban Guerra</h2>
+            <h3 className="text-2xl py-2 dark:text-white">Desarrollador de Aplicaciones Multiplataforma y Web</h3>
             <p className="text-md leading-8 text-gray-800 max-w-xl mx-auto dark:text-white">
-                Soy un desarrollador de aplicaciones con experiencia en, desarrollo e implementación de soluciones para diversas plataformas.
+                Programador con experiencia en, desarrollo e implementación de soluciones para diversas plataformas.
             </p>
         </div>
     )
--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,14 +2,14 @@ function Experience(){
     return (
         <div>
             <h3 className="text-3xl py-1 max-w-xl mx-auto dark:text-white">
-                Mi Experiencia
+                Sobre Mi
             </h3>
             <p className=" text-md py-2 ali leading-8 text-gray-800 max-w-xl mx-auto dark:text-white">
-                Desarrollador frontEnd y BackEnd con mas de 3 años de experiencia y crecimiento constante
-                en el desarrollo de aplicaciones web para dispositivos android e ios mediante <span className="text-blue-500">Apache cordova ,Html , Css, Javascript</span> ,
-                adaptando sus funcionalidades hacia los clientes.
-                Ademas poseo conocimiento en Frameworks como <span className="text-blue-500"> Angular,Ionic, Spring Boot </span > librerias como <span className="text-blue-500">Jquery, React </span>  y BackEnd como <span className="text-blue-500"> NodeJS , Java</span>.
-
+                Desarrollador FrontEnd y BackEnd con mas de 3 años de experiencia y crecimiento constante
+                en el desarrollo de Aplicaciones Web para dispositivos Android e Ios mediante <span className="text-blue-500">Apache cordova ,Html , Css, Javascript ,Jquery</span>
+                y adaptado las misma hacia Apps de escritorio para Windows , Mac y Linux con <span className="text-blue-500"> Framework ElectronJS</span> para
+                mejorar la experiencia de los usuarios.
+                Ademas poseo otros conocimientos como <span className="text-blue-500"> Angular,Ionic, Spring Boot </span > librerias como <span className="text-blue-500">Jquery, React </span>  y BackEnd como <span className="text-blue-500"> NodeJS , Java</span>.
             </p>
         </div>
     )
